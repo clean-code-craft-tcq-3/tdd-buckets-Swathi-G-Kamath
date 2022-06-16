9a60204d41fbe6a2ed6d964576b4631dc9886d9c
Swathi-G-Kamath <swathikamath69@gmail.com>
2022-06-16 20:48:23 +0530

    Update currentRanges.hpp

--- currentRanges.hpp
+++ currentRanges.hpp
@@ -1,5 +1,21 @@
-#include <stdio.h>
+#include <vector>
 
-void bubbleSortAscending(const float* inputArray, int totalReadings, float* sortedArray);
-int detectRanges(const float* readingsArray, int numOfReadings);
-void printArray (float *arrayIndex, int Arraysize);
+using namespace std;
+
+struct currentRanges
+{
+	int m_startIndex;
+	int m_endIndex;
+	int m_totalInRangeReadings;
+	
+	currentRanges()
+	: m_startIndex(0)
+	, m_endIndex(0)
+	, m_totalInRangeReadings(1)
+	{
+	}
+};
+
+std::vector<currentRanges> updateRangeReadings(std::vector<int> periodicCurrentVector);
+void checkForRangeContinuity(std::vector<int> sortedRanges, int currentIndex, currentRanges& currentRanges);
+bool checkForContinuousRange(int currentIndex, int nextIndex);
