56c20083160a80886c7bd22f109b33a2e2eb2296
Swathi-G-Kamath <swathikamath69@gmail.com>
2022-06-30 21:31:50 +0530

    Update tdd_test.cpp

--- tdd_test.cpp
+++ tdd_test.cpp
@@ -24,3 +24,28 @@ TEST_CASE("Test the continuous ranges in given set of current ranges")
   REQUIRE(currentRangesList1[1].m_startIndex == 10);
   REQUIRE(currentRangesList1[1].m_endIndex == 12);
 }
+
+TEST_CASE("Test ConvertAmpsListFromAToD Positive") 
+{
+   std::vector<int> ampsInAnalog {4094, 4000};
+   std::vector<int> ampsInDigital = ConvertAmpsListFromAToD(ampsInAnalog);
+   assert(false == ampsInDigital.empty());
+   REQUIRE(ampsInDigital[0] == 10);
+   REQUIRE(ampsInDigital[1] == 10);
+}
+
+TEST_CASE("Test ConvertAmpsListFromAToD Negative") 
+{
+   std::vector<int> ampsInAnalog {0, 4095};
+   std::vector<int> ampsInDigital = ConvertAmpsListFromAToD(ampsInAnalog);
+   assert(true == ampsInDigital.empty());
+}
+
+TEST_CASE("Test getAmpsInDigital")
+{
+	int ampsInAnalog = 4094;
+	REQUIRE((getAmpsInDigital(ampsInAnalog)) == 10);
+	
+	ampsInAnalog = 1146;
+	REQUIRE((getAmpsInDigital(ampsInAnalog)) == 3);
+}
